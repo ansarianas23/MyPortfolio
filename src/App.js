c29da75ddcfa351d97578ff37bb81fc635d5cf99
Anas7003 <ansariii.5130@gmail.com>
2022-12-17 12:34:16 +0530

    added browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Routes,Route} from "react-router-dom";
+import {Routes,Route, BrowserRouter} from "react-router-dom";
 import About from './routes/About';
 import Home from './routes/Home';
 import Projects from './routes/Projects';
@@ -9,12 +9,14 @@ import Skills from './routes/Skills';
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/project' element={<Projects/>}></Route>
         <Route path='/skills' element={<Skills/>}></Route>
         <Route path='/about' element={<About/>}></Route>
       </Routes>
+    </BrowserRouter>
     </>
   )
 }
